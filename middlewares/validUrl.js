2273bc298c1037d36ee13553f1492b1011959f8d
Javier5Albatros <javieralvarez2.ja@gmail.com>
2021-04-21 00:51:10 +0200

    idk

--- middlewares/validUrl.js
+++ middlewares/validUrl.js
@@ -1,10 +1,15 @@
 const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/i;
 
 const regex = new RegExp(expression);
+const dns = require("dns");
 
 const validUrl = async (req, res, next) => {
   const { url } = req.body;
-  if (!regex.test(url)) {
+  let exists;
+  dns.lookup(url, (err) => {
+    if (err) exists = false;
+  });
+  if (!exists) {
     return res.status(400).json({ error: "invalid url" });
   }
   next();
