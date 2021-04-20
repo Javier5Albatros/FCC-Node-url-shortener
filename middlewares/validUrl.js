83af1463cfc9649ed21c9695f435184fb1660746
Javier5Albatros <javieralvarez2.ja@gmail.com>
2021-04-21 00:40:57 +0200

    Working? wtf

--- middlewares/validUrl.js
+++ middlewares/validUrl.js
@@ -1,12 +1,11 @@
-const urlRegex = require("urlregex");
-
-const expression =
-  "https?://(www.)?[-a-zA-Z0-9@:%._+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)";
+const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/i;
 
 const regex = new RegExp(expression);
 
-const validUrl = (req, res, next) => {
-  if (!req.body.url.match(regex)) {
+const validUrl = async (req, res, next) => {
+  const { url } = req.body;
+  console.log(regex.test(url));
+  if (!match) {
     return res.status(400).json({ error: "invalid url" });
   }
   next();
--- routes/shorturl.js
+++ routes/shorturl.js
@@ -2,17 +2,17 @@ const express = require("express");
 const { check } = require("express-validator");
 const { getUrl, saveUrl } = require("../controllers/shorturl");
 const { errorHandler } = require("../middlewares/errorHandler");
+const { validUrl } = require("../middlewares/validUrl");
 const {
   existingUrlByShort,
   existingUrlByOriginal,
 } = require("../middlewares/existingUrl");
-const { validUrl } = require("../middlewares/validUrl");
 
 const router = express.Router();
 
 router.post(
   "/shorturl",
-  [validUrl, existingUrlByOriginal, errorHandler],
+  [check("url").notEmpty(), validUrl, existingUrlByOriginal, errorHandler],
   saveUrl
 );
 router.get("/shorturl/:url", [existingUrlByShort], getUrl);
